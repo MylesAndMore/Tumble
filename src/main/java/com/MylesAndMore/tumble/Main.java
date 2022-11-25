33ebca4aa95108f4b094df9ea35d42e8946beb20
Myles <mylesandmore9@gmail.com>
2022-11-25 16:04:38 -0600

    add bStats support

--- src/main/java/com/MylesAndMore/tumble/Main.java
+++ src/main/java/com/MylesAndMore/tumble/Main.java
@@ -1,7 +1,18 @@
 package com.MylesAndMore.tumble;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
+    @Override
+    public void onEnable() {
+        // Register our event listener
+        getServer().getPluginManager().registerEvents(new EventListener(), this);
+        // Register our config file
+        this.saveDefaultConfig();
+        // Register bStats
+        int pluginId = 16940;
+        Metrics metrics = new Metrics(this, 16940);
+    }
 
 }
