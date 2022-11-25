caf935bdd12a2cfe5edf9978d6bc42ec0c79a912
Myles <mylesandmore9@gmail.com>
2022-11-25 14:47:28 -0600

    sike we're using spigot now

--- src/main/java/com/MylesAndMore/tumble/Main.java
+++ src/main/java/com/MylesAndMore/tumble/Main.java
@@ -1,13 +1,7 @@
 package com.MylesAndMore.tumble;
 
-import com.google.inject.Inject;
-import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.proxy.ProxyServer;
-import org.slf4j.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
 
-@Plugin(id = "tumble", name = "tumble", version = "0.0.1-SNAPSHOT",
-        url = "https://github.com/MylesAndMore/tumble", description = "A Minecraft: Java Edition plugin recreating the Tumble minigame from Minecraft: Xbox 360 Edition. ", authors = {"MylesAndMore"})
+public class Main extends JavaPlugin{
 
-public class Main {
-    
 }
