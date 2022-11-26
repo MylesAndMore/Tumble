b36d422c52e1445833ce84f9e334598f0b180785
Myles <mylesandmore9@gmail.com>
2022-11-26 13:19:42 -0600

    oopsies forgot to add perms

--- src/main/java/com/MylesAndMore/tumble/commands/SetWorldConfig.java
+++ src/main/java/com/MylesAndMore/tumble/commands/SetWorldConfig.java
@@ -19,38 +19,45 @@ public class SetWorldConfig implements CommandExecutor {
         String worldType = args[1];
         // Catch for null arguments
         if (args.length > 0) {
-            // Check if the world type is lobby
-            if (Objects.equals(worldType, "lobby")) {
-                // Check if the world is actually a world on the server
-                if (Bukkit.getWorld(world) != null) {
-                    // Set the specified value of the world in the config under lobbyWorld
-                    plugin.getConfig().set("lobbyWorld", world);
-                    // Save said config
-                    plugin.saveConfig();
-                    // Feedback
-                    sender.sendMessage(ChatColor.GREEN + "Lobby world successfully linked: " + ChatColor.GRAY + world);
-                    sender.sendMessage(ChatColor.GREEN + "Run /tumble:reload for the changes to take effect.");
+            // Check if sender has perms to run command
+            if (!sender.hasPermission("tumble.setworld")){
+                // Check if the world type is lobby
+                if (Objects.equals(worldType, "lobby")) {
+                    // Check if the world is actually a world on the server
+                    if (Bukkit.getWorld(world) != null) {
+                        // Set the specified value of the world in the config under lobbyWorld
+                        plugin.getConfig().set("lobbyWorld", world);
+                        // Save said config
+                        plugin.saveConfig();
+                        // Feedback
+                        sender.sendMessage(ChatColor.GREEN + "Lobby world successfully linked: " + ChatColor.GRAY + world);
+                        sender.sendMessage(ChatColor.GREEN + "Run /tumble:reload for the changes to take effect.");
+                    }
+                    // Feedback for if the world doesn't exist
+                    else {
+                        sender.sendMessage(ChatColor.RED + "Failed to find a world named " + ChatColor.GRAY + world);
+                    }
                 }
-                // Feedback for if the world doesn't exist
+                // Check if the world type is game
+                else if (Objects.equals(args[1], "game")) {
+                    if (Bukkit.getWorld(world) != null) {
+                        plugin.getConfig().set("gameWorld", world);
+                        plugin.saveConfig();
+                        sender.sendMessage(ChatColor.GREEN + "Game world successfully linked: " + ChatColor.GRAY + world);
+                        sender.sendMessage(ChatColor.GREEN + "Run /tumble:reload for the changes to take effect.");
+                    }
+                    else {
+                        sender.sendMessage(ChatColor.RED + "Failed to find a world named " + ChatColor.GRAY + world);
+                    }
+                }
+                // Feedback for if lobby or game wasn't entered
                 else {
-                    sender.sendMessage(ChatColor.RED + "Failed to find a world named " + ChatColor.GRAY + world);
+                    sender.sendMessage(ChatColor.RED + "Allowed world types are " + ChatColor.GRAY + "lobby " + ChatColor.RED + "and " + ChatColor.GRAY + "game" + ChatColor.RED + ".");
                 }
             }
-            // Check if the world type is game
-            else if (Objects.equals(args[1], "game")) {
-                if (Bukkit.getWorld(world) != null) {
-                    plugin.getConfig().set("gameWorld", world);
-                    plugin.saveConfig();
-                    sender.sendMessage(ChatColor.GREEN + "Game world successfully linked: " + ChatColor.GRAY + world);
-                    sender.sendMessage(ChatColor.GREEN + "Run /tumble:reload for the changes to take effect.");
-                }
-                else {
-                    sender.sendMessage(ChatColor.RED + "Failed to find a world named " + ChatColor.GRAY + world);
-                }
-            }
-            // Feedback for if lobby or game wasn't entered
+            // Feedback for if sender has no perms
             else {
-                sender.sendMessage(ChatColor.RED + "Allowed world types are " + ChatColor.GRAY + "lobby " + ChatColor.RED + "and " + ChatColor.GRAY + "game" + ChatColor.RED + ".");
+                sender.sendMessage(ChatColor.RED + plugin.getConfig().getString("permissionMessage"));
             }
         }
         // Feedback for if no args were entered
