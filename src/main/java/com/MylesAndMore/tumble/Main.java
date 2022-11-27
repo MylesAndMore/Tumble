b49d0b702b134550cfd20aba88de91ab8354fe01
Myles <mylesandmore9@gmail.com>
2022-11-26 22:50:47 -0600

    add null config check in Main.java

--- src/main/java/com/MylesAndMore/tumble/Main.java
+++ src/main/java/com/MylesAndMore/tumble/Main.java
@@ -4,6 +4,7 @@ import com.MylesAndMore.tumble.commands.ReloadCommand;
 import com.MylesAndMore.tumble.api.Metrics;
 import com.MylesAndMore.tumble.commands.SetWorldConfig;
 import com.MylesAndMore.tumble.commands.StartGame;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -21,6 +22,16 @@ public class Main extends JavaPlugin{
         // Register bStats
         int pluginId = 16940;
         Metrics metrics = new Metrics(this, 16940);
+
+        // Check if worlds are null in config
+        if (PluginManager.getPlugin().getConfig().getString("gameWorld") == null) {
+            Bukkit.getServer().getLogger().warning("It appears you have not configured a game world for Tumble.");
+            Bukkit.getServer().getLogger().warning("If this is your first time running the plugin, you may disregard this message.");
+        }
+        if (PluginManager.getPlugin().getConfig().getString("lobbyWorld") == null) {
+            Bukkit.getServer().getLogger().warning("It appears you have not configured a lobby world for Tumble.");
+            Bukkit.getServer().getLogger().warning("If this is your first time running the plugin, you may disregard this message.");
+        }
     }
 
     public void onDisable() {
