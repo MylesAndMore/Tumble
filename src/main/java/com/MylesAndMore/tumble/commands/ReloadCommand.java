afacf614ebaf0c9e9b0ebf7193cfc13d86f18169
Myles <mylesandmore9@gmail.com>
2022-11-27 14:19:38 -0600

    reformat if/else to reflect commonality

--- src/main/java/com/MylesAndMore/tumble/commands/ReloadCommand.java
+++ src/main/java/com/MylesAndMore/tumble/commands/ReloadCommand.java
@@ -10,14 +10,14 @@ public class ReloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         // Check if the sender has perms to run command
-        if (!sender.hasPermission("tumble.reload")) {
-            // If sender does not have permission, display them the permissionMessage from the config
-            sender.sendMessage(ChatColor.RED + PluginManager.getPlugin().getConfig().getString("permissionMessage"));
-        }
-        else {
+        if (sender.hasPermission("tumble.reload")) {
             // If sender does have permission, reload the plugin's config and display a confirmation message
             PluginManager.getPlugin().reloadConfig();
             sender.sendMessage(ChatColor.GREEN + "Tumble configuration reloaded successfully.");
+        }
+        else {
+            // If sender does not have permission, display them the permissionMessage from the config
+            sender.sendMessage(ChatColor.RED + PluginManager.getPermissionMessage());
         }
         return true;
     }
