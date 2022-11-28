bc78a63afcfe4545de8c89673d99bf8b4dd926bf
Myles <mylesandmore9@gmail.com>
2022-11-27 20:50:45 -0600

    add cyl support in generator

--- src/main/java/com/MylesAndMore/tumble/api/Generator.java
+++ src/main/java/com/MylesAndMore/tumble/api/Generator.java
@@ -5,7 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.World;
 
 public class Generator {
-    public static void generateLayer(Location center, int radius, Material material) {
+    public static void generateLayer(Location center, int radius, int height, Material material) {
         int Cx = center.getBlockX();
         int Cy = center.getBlockY();
         int Cz = center.getBlockZ();
@@ -13,11 +13,13 @@ public class Generator {
 
         int rSq = radius * radius;
 
-        for (int x = Cx - radius; x <= Cx + radius; x++) {
-            for (int z = Cz - radius; z <= Cz + radius; z++) {
-                if ((Cx - x) * (Cx - x) + (Cz - z) * (Cz - z) <= rSq) {
-                    Location block = new Location(world, x, Cy, z);
-                    world.getBlockAt(x, Cy, z).setType(material);
+        for (int y = Cy; y < Cy + height; y++) {
+            for (int x = Cx - radius; x <= Cx + radius; x++) {
+                for (int z = Cz - radius; z <= Cz + radius; z++) {
+                    if ((Cx - x) * (Cx - x) + (Cz - z) * (Cz - z) <= rSq) {
+                        Location block = new Location(world, x, y, z);
+                        world.getBlockAt(x, y, z).setType(material);
+                    }
                 }
             }
         }
