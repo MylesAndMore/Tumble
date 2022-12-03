8273f0654e8dac3ff2ce0da422afce12c54cd0e3
Myles <mylesandmore9@gmail.com>
2022-12-02 23:28:10 -0600

    add PlayerDeathEvent for use in Game

--- src/main/java/com/MylesAndMore/tumble/EventListener.java
+++ src/main/java/com/MylesAndMore/tumble/EventListener.java
@@ -3,12 +3,13 @@ package com.MylesAndMore.tumble;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class EventListener implements Listener{
     @EventHandler
-    public void PlayerJoinEvent(PlayerJoinEvent event){
+    public void PlayerJoinEvent(PlayerJoinEvent event) {
         // On a PlayerJoinEvent, check if the config is set to hide the join/leave messages
         // If true, null out the join message (which just makes it so that there is no message)
         // If false, nothing will happen, and the default message will display
@@ -26,12 +27,24 @@ public class EventListener implements Listener{
     }
 
     @EventHandler
-    public void PlayerQuitEvent(PlayerQuitEvent event){
+    public void PlayerQuitEvent(PlayerQuitEvent event) {
         // On a PlayerQuitEvent, check if the config is set to hide the join/leave messages
         // If true, null out the quit message (which just makes it so that there is no message)
         // If false, nothing will happen, and the default message will display
         if (TumbleManager.getPlugin().getConfig().getBoolean("hideJoinLeaveMessages")) {
             event.setQuitMessage(null);
+        }
+    }
+
+    @EventHandler
+    public void PlayerDeathEvent(PlayerDeathEvent event) {
+        // On a PlayerDeathEvent, check to make sure the gameWorld is defined,
+        if (TumbleManager.getGameWorld() != null) {
+            // then check to see if the player died in the gameWorld,
+            if (event.getEntity().getWorld() == Bukkit.getWorld(TumbleManager.getGameWorld())) {
+                // then pass this off to the Game
+                Game.getGame().playerDeath(event.getEntity());
+            }
         }
     }
 }
